70305cc8cb1620598b7eef37d6da0008d351d50a
omar jandali <omar jandali>
2016-10-06 22:26:08 -0700

    initialized the database

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
 import psycopg2
 
-
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
-    return psycopg2.connect("dbname=tournament")
-
+    connection = psycopg2.connect("dbname=tournament")
+    return connection
 
 def deleteMatches():
     """Remove all the match records from the database."""
-
+    cursor = connect().cursor
+    cursor.execute("delete * from matches")
 
 def deletePlayers():
     """Remove all the player records from the database."""
-
+    cursor = connect().cursor
+    cursor.execute("delete * from players")
 
 def countPlayers():
     """Returns the number of players currently registered."""
@@ -25,10 +26,10 @@ def countPlayers():
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
@@ -56,16 +57,16 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,35 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+--------------------------------------------------------------------------------
 
+-- the following will clear all of the databases if they already exists
+drop database if exists tournament;
+
+-- the is the creation of the entire tournaments database
+create database tournament;
+
+-- the following are a list of tables that are going to be used in this project
+
+-- this table will store all of the players in the tournament
+create table players(
+  id serial primary key,
+  name text
+);
+
+create table records(
+  id int references players(id),
+  wins int,
+  loses int
+)
+
+create table matches(
+  rotation serial primary key,
+  player_1 int references player(id),
+  player_2 int references player(id)
+)
+
+create view standings as
+  select players.id, records.wins, records.loses
+    from players and records
+    order by records.wins and players.id asc

cb9fdc633603a77550adac491d5210bbf229a357
omar jandali <omar jandali>
2016-10-19 15:13:04 -0700

    setup the database and server

--- vagrant/tastebuds/setup_database.py
+++ vagrant/tastebuds/setup_database.py
@@ -8,34 +8,37 @@ This file is the property of OmnaCore and was written by Omar Jandali
 
 import os
 import sys
-from sqlalchemy import Conumn, String, Integer, ForeignKey, Index
+from sqlalchemy import Column, String, Integer, ForeignKey, Index
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+#The following table will save all of the restaurants and their record id
 class Restaurants(Base):
     __tablename__ = 'restaurants'
 
-    id = Column(Integer, Primary_Key = True)
-    name = Column(String(100), nullable = False, Unique = True, Index = True)
+    id = Column(Integer, primary_key = True)
+    name = Column(String(100), nullable = False, unique = True, index = True)
 
+# the following class will store all of the restaurants general informaiton
 class Restaurants_Info(Base):
     __tablename__ = 'restaurants_info'
 
-    id = Column(Integer, Primary_Key = True)
+    id = Column(Integer, primary_key = True)
     food_type = Column(String(100), nullable = False)
     avg_price = Column(Integer(5), nullable = False)
-    city = Column(String(100), nullable = False, Index = True)
+    city = Column(String(100), nullable = False, index = True)
     state = Column(String(3), nullable = False)
     restaurants_id = Column(Integer(100), ForeignKey('restaurants.id'))
     restaurants = relationship(Restaurants)
 
+# the following table contains all of the different ratings that cover most aspects of the restaurant
 class Restaurants_Rating(Base):
     __tablename__ = 'restaurants_rating'
 
-    id = Column(Integer, Primary_Key = True)
+    id = Column(Integer, primary_key = True)
     restaurants_rating = Column(Integer(2), nullable = True)
     food_rating = Column(Integer(2), nullable = True)
     service_rating = Column(Integer(2), nullable = True)
@@ -44,26 +47,29 @@ class Restaurants_Rating(Base):
     restaurants_id = Column(Integer(100), ForeignKey('restaurants.id'))
     restaurants = relationship(Restaurants)
 
+#The following table will asign and save the name and record id of each dish
 class Dishes(Base):
     __tablename__ = 'dishes'
 
-    id = Column(Integer, Primary_Key = True)
-    name = Column(String(100), nullable = True, Unique = True, Index = True)
+    id = Column(Integer, primary_key = True)
+    name = Column(String(100), nullable = True, unique = True, index = True)
 
+#The following table will store and track all of the dishes general information
 class Dishes_Info(Base):
     __tablename__ = 'dishes_info'
 
-    id = Column(Integer, Primary_Key = True)
+    id = Column(Integer, primary_key = True)
     course = Column(String(100), nullable = True)
     price = Column(Integer(6), nullable = True)
     description = Column(String(200), nullable = True)
     dishes_id = Column(Integer, ForeignKey('dishes.id'))
     dishes = relationship(Dishes)
 
+#the following table will keep track of each dishes rating aspects
 class Dishes_Rating(Base):
     __tablename__ = 'dishes_rating'
 
-    id = Column(Integer, Primary_Key = True)
+    id = Column(Integer,primary_key = True)
     dishes_rating = Column(Integer(2), nullable = True)
     flavor_rating = Column(Integer(2), nullable = True)
     texture_rating = Column(Integer(2), nullable = True)
@@ -75,3 +81,27 @@ class Dishes_Rating(Base):
 engine = create_engine('sqlite:///tastebuds.db')
 
 Base.metadata.create_all(engine)
+
+"""
+Potential changes:
+    current - the user will put a rating for each of the different aspects of a dish or restaurant that
+    they chose as well as a over rating [1-5]
+
+        Restaurant [ 1-5 ]
+        food [ 1-5 ]
+        service [ 1-5 ]
+        location [ 1-5 ]
+        access [ 1-5 ]
+        price [ 1-5 ]
+
+    possibility - change to display a small chart to base the rating over and just one inputted ratio rather
+    than several different ratios.
+
+        food (0-.5-1)
+        service (0-.5-1)
+        location (0-.5-1)
+        access (0-.5-1)
+        pricing (0-.5-1)
+        Restaurant [ 1-5 ]
+
+"""
---
+++ vagrant/tastebuds/setup_server.py
@@ -0,0 +1,31 @@
+"""
+This file is the property of OmnaCore and was written by Omar Jandali
+
+-- This file is the initial server setup up file which includes:
+    -- it will contain the Flask framework that will cover the following:
+        -- all the routes and urls for the site
+        -- all of the functions that will be handled for each of the pages
+        -- all of the forms that will be used to CRUD the database records
+"""
+
+from flask import Flask, render_template, request, redirect, url_for, flash
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from setup_database import Base, Restaurants, Restaurants_Info, Restaurants_Rating, Dishes, Dishes_Info, Dishes_Rating
+
+app = Flask(__name__)
+
+engine = create_engine('sqlite:///tastebuds.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+@app.route('/test')
+def testingPage():
+    return 'This is just a test page to get the server running'
+
+if __name__ == '__main__':
+    app.secret_key = "Secret_Key"
+    app.debug = True
+    app.run(host = '0.0.0.0', port = 9090)

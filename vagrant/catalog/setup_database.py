4cdd3b6998d5e8f10f05d90d076e0450d6da5c07
omar jandali <omar jandali>
2016-11-08 00:36:05 -0800

    Initial submission of udacity project

--- vagrant/catalog/setup_database.py
+++ vagrant/catalog/setup_database.py
@@ -7,28 +7,56 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-class Categories(Base):
-  __tablename__ = 'categories'
+class Users(Base):
+    __tablename__ = 'users'
 
-  id = Column(Integer, primary_key = True)
-  category = Column(String(100), nullable = False, index = True, unique = True)
+    id = Column(Integer, primary_key = True)
+    username = Column(String(100), index = True)
+    email = Column(String(225), nullable = False)
+    profile_pic = Column(String(225), nullable = False)
+
+    @property
+    def serialize(self):
+        return{
+            'id': self.id,
+            'username': self.username,
+            'email': self.email,
+            'profile_picture': self.profile_pic,
+        }
+
+class Categories(Base):
+    __tablename__ = 'categories'
+
+    id = Column(Integer, primary_key = True)
+    category = Column(String(100), nullable = False, index = True, unique = True)
+    users_id = Column(Integer, ForeignKey('users.email'))
+    users = relationship(Users)
+
+    @property
+    def serialize(self):
+        return{
+            'category': self.category,
+        }
 
 class Items(Base):
-  __tablename__ = 'items'
+    __tablename__ = 'items'
 
-  id = Column(Integer, primary_key = True)
-  title = Column(String(100), unique = True)
-  description = Column(String(225))
-  category_name = Column(String(100), ForeignKey('categories.category'))
-  category = relationship(Categories)
+    id = Column(Integer, primary_key = True)
+    title = Column(String(100), unique = True)
+    description = Column(String(225))
+    category_name = Column(String(100), ForeignKey('categories.category'))
+    category = relationship(Categories)
+    users_id = Column(Integer, ForeignKey('users.email'))
+    users = relationship(Users)
 
-class Users(Base):
-  __tablename__ = 'users'
-
-  id = Column(Integer, primary_key = True)
-  username = Column(String(100), index = True)
-  email = Column(String(225), nullable = False)
-  profile_pic = Column(String(225), nullable = False)
+    @property
+    def serialize(self):
+        return{
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+            'category': self.category_name,
+        }
 
 engine = create_engine('sqlite:///catelogs.db')
 
--- vagrant/catalog/setup_server.py
+++ vagrant/catalog/setup_server.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from setup_database import Base, Categories, Items
+from setup_database import Base, Categories, Items, Users
 
 app = Flask(__name__)
 
@@ -30,39 +30,56 @@ def TestPage():
 
 @app.route('/home')
 def HomePage():
-  categories = session.query(Categories).order_by(Categories.id)
-  items = session.query(Items).order_by(Items.id)
-  return render_template('homePage.html', categories = categories, items = items)
-
-"""
--- uncomment the following  route if you would like to add more categories to the list --
-
-@app.route('/createcategory', methods=['GET', 'POST'])
-def CreateCategory():
-    if request.method == 'POST':
-        newCategory = Categories(category = request.form['name'])
-        session.add(newCategory)
-        session.commit()
-        return redirect(url_for('HomePage'))
-    return render_template('createCategory.html')
-
-
     categories = session.query(Categories).order_by(Categories.id)
     items = session.query(Items).order_by(Items.id)
-
-"""
+    if 'username' not in login_session:
+        return render_template('publicHomePage.html', categories = categories, items = items)
+    return render_template('homePage.html', categories = categories, items = items)
 
 @app.route('/createitem', methods = ['GET', 'POST'])
 def AddItem():
+    #if 'username' not in login_session:
+    #    return redirect('/login')
     categories = session.query(Categories).order_by(Categories.id)
     if request.method == 'POST':
         newItem = Items(title = request.form['title'],
                         description = request.form['description'],
-                        category_name = request.form['category_name'])
+                        category_name = request.form['category_name'],
+                        users_id = 1)
         session.add(newItem)
         session.commit()
         return redirect(url_for('HomePage'))
     return render_template('createItems.html', categories = categories)
+
+@app.route('/<string:item_category>/<string:item_title>/edit', methods=['GET', 'POST'])
+def EditItem(item_category, item_title):
+    if 'username' not in login_session:
+        return redirect('/login')
+    categories = session.query(Categories).order_by(Categories.id)
+    editedItem = session.query(Items).filter_by(title = item_title).one()
+    if request.method == 'POST':
+        if request.form['title']:
+            editedItem.title = request.form['title']
+        if request.form['description']:
+            editedItem.description = request.form['description']
+        if request.form['category_name']:
+            editedItem.category_name = request.form['category_name']
+        session.add(editedItem)
+        session.commit()
+        return redirect(url_for('HomePage'))
+    return render_template('editItem.html', item_category = item_category, categories = categories, editedItem = editedItem)
+
+@app.route('/<string:item_category>/<string:item_title>/delete', methods=['GET', 'POST'])
+def DeleteItem(item_category, item_title):
+    categories = session.query(Categories).order_by(Categories.id)
+    deleteItem = session.query(Items).filter_by(title = item_title).one()
+    if 'username' not in login_session:
+        return redirect('/login')
+    if request.method == 'POST':
+        session.delete(deleteItem)
+        session.commit()
+        return redirect(url_for('HomePage'))
+    return render_template('deleteItem.html', item_category = item_category, deleteItem = deleteItem, categories = categories)
 
 @app.route('/catalog/<string:category>/items')
 def ShowItems(category):
@@ -72,14 +89,18 @@ def ShowItems(category):
 
 @app.route('/catalog/<string:category>/<string:item>')
 def SelectedItem(category, item):
+    categories = session.query(Categories).order_by(Categories.id)
     selecteditem = session.query(Items).filter_by(title = item).one()
-    return render_template('ShowSelectedItem.html', item = selecteditem, category = category)
+    if 'username' not in login_session:
+        return render_template('publicShowSelectedItem.html', item = selecteditem, category = category,  categories = categories)
+    return render_template('ShowSelectedItem.html', item = selecteditem, category = category, categories = categories)
 
 @app.route('/login')
 def Login():
+    categories = session.query(Categories).order_by(Categories.id)
     state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
     login_session['state'] = state
-    return render_template('login.html', STATE = state)
+    return render_template('login.html', STATE = state, categories = categories)
 
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
@@ -176,12 +197,13 @@ def gconnect():
 @app.route('/gdisconnect')
 def gdisconnect():
     credentials = login_session.get('credentials')
+    print credentials
     if credentials is None:
         response = make_response(json.dumps('Current user not connection'), 401)
         response.headersp['Content-Type'] = 'application/json'
         return response
     #access_token = credentials.access_token
-    url = url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % credentials
+    url = 'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' % credentials
     h = httplib2.Http()
     result = h.request(url, 'GET')[0]
 
@@ -199,6 +221,45 @@ def gdisconnect():
         response = make_response(json.dumps('Failed to revoke token for given user'), 400)
         response.headers['Content-Type'] = 'application/json'
         return response
+
+def CreateUser(login_session):
+    newUser = Users(username = login_session['username'],
+                    email = login_session['email'],
+                    profile_pic = login_session['picture'])
+    session.add(newUser)
+    session.commit()
+    user = session.query(Users).filter_by(email = login_session['email']).one()
+    return user.id
+
+def getUserInfo(user_id):
+    user = session.query(Users).filter_by(id = user_id).one()
+    return user
+
+def getUserId(email):
+    try:
+        user = session.query(Users).filter_by(email = email).one()
+        return user.id
+    except:
+        return None
+
+"""
+-- uncomment the following  route if you would like to add more categories to the list --
+
+@app.route('/createcategory', methods=['GET', 'POST'])
+def CreateCategory():
+    if request.method == 'POST':
+        newCategory = Categories(category = request.form['name'],
+                                 users_id = 1)
+        session.add(newCategory)
+        session.commit()
+        return redirect(url_for('HomePage'))
+    return render_template('createCategory.html')
+"""
+@app.route('/catalog/<string:categories_name>/items/JSON')
+def categoryItemsJSON(categories_name):
+    categories = session.query(Categories).filter_by(category = categories_name).one()
+    items = session.query(Items).filter_by(category_name = categories_name).all()
+    return jsonify(CategoryItems = [i.serialize for i in items])
 
 if __name__ == '__main__':
     app.secret_key = "Secret_Key"
